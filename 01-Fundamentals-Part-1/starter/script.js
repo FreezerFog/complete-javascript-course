00ef3c5fe9fe71ccdbb0f1f5933fa98ed6639c22
FreezerFog <djwaccount+github@protonmail.com>
2021-12-09 17:13:55 -0800

    Complete exercise 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,42 @@
+
+/*
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+let markBMI = markMass / (markHeight ** 2);
+let johnBMI = johnMass / (johnHeight ** 2);
+let markHigherBMI;
+let message;
+markHigherBMI = markBMI > johnBMI ? true : false;
+if (markHigherBMI) {
+  message = `Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`;
+} else {
+  message = `John's BMI (${johnBMI}) is higher than Marks's (${markBMI})!`;
+}
+console.log(message);
+*/
+
+/*
+let dolphins = (96 + 108 + 89) / 3;
+let koalas = (88 + 91 + 110) / 3;
+let minimumToWin = 100;
+let winner;
+if (dolphins < minimumToWin && koalas < minimumToWin) {
+  winner = "Both teams lose. No trophys!"
+} else if (dolphins === koalas) {
+  winner = "Draw! Both teams win trophy"; 
+} else {
+  winner = dolphins > koalas ? "Dolphins win trophy!" : "Koalas win trophy!";
+}
+console.log(winner);
+*/
+
+// Coding Challenge #4
+/*
+let bill = 275;
+let tip;
+tip = bill >= 50 && bill <= 300 ? (bill * .15) : (bill * .20);
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value was $${bill + tip}.`);
+*/
+
