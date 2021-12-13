8953988fa17cd79320a670a3b595343f3a73e53a
FreezerFog <djwaccount+github@protonmail.com>
2021-12-12 17:24:50 -0800

    Complete exercise 3

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xyz = '23';
+if (xyz === 23) {
+  console.log(23);
+}
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log();
+
+console.log(xyz);
+console.log();
+
+console.log();
+
+console.log();
+
+//"Derek loves caryn"
+
+// TODO
+// BUG
+// COMPLETED
+// FIXME
+/*
+PSUEDOCODE
++ TEST THE
+- Test the
+- Watch the level
+    - kklfsjsakfld
+    - 
+*/
