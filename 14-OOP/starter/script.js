501cee629639d0a036fffb0281327dbec8c63f6b
FreezerFog <djwaccount+github@protonmail.com>
2022-03-10 18:08:12 -0800

    Complete video 208

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Only difference between normal function and constructor function is the use of the 'new' operator
+
+function Person(firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Methods
+  // Never do this
+  // Makes copy of function in every instance, very inefficient!
+  // this.calcAge = function () {
+  //   console.log(2022 - this.birthYear);
+  // };
+}
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(jonas);
+console.log(matilda);
+console.log(jack);
+
+// instanceof
+// Compares an object with a constructor function
+// Returns true if object is an instance of the compared constructor function
+console.log(jonas instanceof Person);
