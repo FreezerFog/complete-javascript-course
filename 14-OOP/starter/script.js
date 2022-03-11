c86eaaa931e1d7ba7d006e034fc56afce11131c8
FreezerFog <djwaccount+github@protonmail.com>
2022-03-10 20:39:20 -0800

    Complete video 209

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -26,3 +26,32 @@ console.log(jack);
 // Compares an object with a constructor function
 // Returns true if object is an instance of the compared constructor function
 console.log(jonas instanceof Person);
+
+////// Prototypes //////
+// Setting a function on the prototype so that each instance can use it
+// Avoids creating a copy of function for each instance
+Person.prototype.calcAge = function () {
+  return 2022 - this.birthYear;
+};
+console.log(`Jonas Age: ${jonas.calcAge()}`);
+
+// __proto__
+// Returns the object's prototype
+// Call on an object to check its prototype
+console.log(jonas.__proto__);
+// Person.prototype is NOT the prototype of PERSON, but instead is the prototype of every INSTANCE OF Person.
+// The Person.prototype will be the same as the prototype of every instance of Person.
+console.log(jonas.__proto__ === Person.prototype); // Returns TRUE
+console.log(Person.prototype.isPrototypeOf(jonas)); // Returns TRUE
+console.log(Person.prototype.isPrototypeOf(Person)); // Returns FALSE
+// The above is the result of poor naming. Person.prototype suggests the prototype is for Person, not its linked objects.
+// A better name would be something like Person.prototypeOfLinkedObjects
+
+// Setting a property on the prototype
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species);
+console.log(matilda.species);
+// Does NOT set the property on the object itself
+console.log(jonas);
+console.log(jonas.hasOwnProperty('firstName')); // True
+console.log(jonas.hasOwnProperty('species')); // False
