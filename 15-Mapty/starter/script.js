570e8e00f9e42e8074bd1a451a16324fa1100a4c
FreezerFog <djwaccount+github@protonmail.com>
2022-04-05 15:17:59 -0700

    Complete video 235 & 236

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,6 +3,7 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+////// DOM ELEMENTS //////
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,40 +12,70 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+////// GLOBAL VARIABLES //////
+let map, mapEvent;
+
+////// CLASSES //////
+
+////// EVENT HANDLERS //////
 // Geolocation API, gained from the browser
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
-      console.log(mapEvent);
-
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
+    // Handling clicks on map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      // Show form by removing hidden class
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   function () {
     alert('Could not get position. Please check your browser settings.');
   }
 );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  clearInputFields();
+
+  // Display Marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+////// FUNCTIONS //////
+function clearInputFields() {
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+}
