e0fe354466943669522ae556ea43f3e1dce9be3b
FreezerFog <djwaccount+github@protonmail.com>
2022-04-07 20:23:41 -0700

    Complete video 250

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -44,6 +44,65 @@ function getCountryData(country) {
   });
 }
 
-getCountryData('chile');
-getCountryData('usa');
-getCountryData('new zealand');
+function renderCountry(data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+// getCountryData('chile');
+// getCountryData('usa');
+// getCountryData('new zealand');
+
+function getCountryAndNeighbor(country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+    // console.log(request.responseText); // Same as above
+
+    // Convert response to JSON, and destructure JSON array result into the data variable
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country 2
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render country 1
+      renderCountry(data2, 'neighbour');
+    });
+  });
+}
+
+getCountryAndNeighbor('portugal');
