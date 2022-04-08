3404a1390466ee6303f44db829fa0925e3a7050d
FreezerFog <djwaccount+github@protonmail.com>
2022-04-07 19:36:55 -0700

    Complete video 248

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,47 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// API URL TO USE
+// https://restcountries.com/v2/
+
 ///////////////////////////////////////
+
+// OLD SCHOOL AJAX WAY
+function getCountryData(country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+    // console.log(request.responseText); // Same as above
+
+    // Convert response to JSON, and destructure JSON array result into the data variable
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // const { name, capital } = JSON.parse(this.responseText);
+    // console.log(name, capital);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+}
+
+getCountryData('chile');
+getCountryData('usa');
+getCountryData('new zealand');
