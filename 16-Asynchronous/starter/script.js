505b09f5f197309d58b6876145f6aa8b7c1e1f34
FreezerFog <djwaccount+github@protonmail.com>
2022-04-08 22:26:44 -0700

    Complete video 254: Handling Rejected Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,9 +7,30 @@ const countriesContainer = document.querySelector('.countries');
 // https://restcountries.com/v2/
 
 ///////////////////////////////////////////////////////////
-// VIDEO253 - Chaining Promises
+// VIDEO254 - Handling Rejected Promises
 
-// fetch() API refactored for simplicity using arrow functions
+// 2 ways to deal with rejected promises:
+// Option 1: then() callbacks with each then() method
+// Option 2: catch() at end of fetch() chain to handle all errors
+
+// then(), catch(), and finally() run on fetch() when promise is settled as:
+// then() only on fulfilled
+// catch() only on rejected
+// finally() always!
+
+// finally()
+// This callback function will always execute on a fetch() request
+// Often used for hiding 'spinners' that indicate a website is loading something
+
+// Option 1 Example
+// then() accepts 2 callback functions. The 1st for fulfilled, 2nd for rejected
+//    .then(
+//      response => response.json(),
+//      err => alert(err)
+//    )
+
+// Option 2 Example
+// catch() as seen below
 function getCountryData(country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
@@ -20,7 +41,14 @@ function getCountryData(country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} :(`);
+      renderError(`Something went wrong: ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      renderContainer();
+    });
 }
 
 function renderCountry(data, className = '') {
@@ -40,7 +68,16 @@ function renderCountry(data, className = '') {
   `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
+}
+
+function renderContainer() {
   countriesContainer.style.opacity = 1;
 }
 
-getCountryData('germany');
+function renderError(msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+}
+
+btn.addEventListener('click', function () {
+  getCountryData('germany');
+});
