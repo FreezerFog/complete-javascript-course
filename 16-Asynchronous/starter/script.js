7264c3f153ada3c21abc381ca931a9b0ed4d6dd8
FreezerFog <djwaccount+github@protonmail.com>
2022-04-08 21:40:12 -0700

    Complete video 251-252 Promises and the Fetch API & Consuming Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,26 +6,38 @@ const countriesContainer = document.querySelector('.countries');
 // API URL TO USE
 // https://restcountries.com/v2/
 
-///////////////////////////////////////
+///////////////////////////////////////////////////////////
+// VIDEO251 - Promises and the Fetch API
+// VIDEO252 - Consuming Promises
 
-// OLD SCHOOL AJAX WAY
+// fetch() API with explanations
+// function getCountryData(country) {
+//   // fetch() returns a promise, which will first be pending, but will then be fulfilled
+//   // then() allows us to deal with the settled states of fulfilled and rejected
+//   // then() accepts a callback function to be executed when promise is settled
+//   // then() callback function accepts the response as it's only parameter
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       // json() is a method that's available on all responses from the fetch method
+//       // json() is also an asynchronous function. It returns a new promise with its data.
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       // This 2nd then() allows us to use data from previous then() json() call
+//       renderCountry(data[0]);
+//     });
+// }
+
+// fetch() API refactored for simplicity using arrow functions
 function getCountryData(country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+}
 
-  request.addEventListener('load', function () {
-    console.log(this.responseText);
-    // console.log(request.responseText); // Same as above
-
-    // Convert response to JSON, and destructure JSON array result into the data variable
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    // const { name, capital } = JSON.parse(this.responseText);
-    // console.log(name, capital);
-
-    const html = `
-  <article class="country">
+function renderCountry(data, className = '') {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -39,8 +51,25 @@ function getCountryData(country) {
   </article>
   `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+getCountryData('portugal');
+
+///////////////////////////////////////////////////////////
+// VIDEO248 - AJAX with XMLHttpRequest
+// VIDEO250 - Callback Hell
+/*
+// OLD SCHOOL AJAX WAY
+function getCountryData(country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
   });
 }
 
@@ -68,6 +97,7 @@ function renderCountry(data, className = '') {
 // getCountryData('usa');
 // getCountryData('new zealand');
 
+// Callback Hell!
 function getCountryAndNeighbor(country) {
   // AJAX call country 1
   const request = new XMLHttpRequest();
@@ -75,18 +105,14 @@ function getCountryAndNeighbor(country) {
   request.send();
 
   request.addEventListener('load', function () {
-    console.log(this.responseText);
-    // console.log(request.responseText); // Same as above
-
-    // Convert response to JSON, and destructure JSON array result into the data variable
     const [data] = JSON.parse(this.responseText);
-    console.log(data);
 
     // Render country 1
     renderCountry(data);
 
     // Get neighbor country 2
     const [neighbour] = data.borders;
+
     if (!neighbour) return;
 
     // AJAX call country 2
@@ -95,14 +121,13 @@ function getCountryAndNeighbor(country) {
     request2.send();
 
     request2.addEventListener('load', function () {
-      console.log(this.responseText);
       const data2 = JSON.parse(this.responseText);
-      console.log(data2);
 
-      // Render country 1
+      // Render country 2
       renderCountry(data2, 'neighbour');
     });
   });
 }
 
 getCountryAndNeighbor('portugal');
+*/
