5c409cbb1435c8bf4cf90af1c99a904447633da7
FreezerFog <djwaccount+github@protonmail.com>
2022-04-27 17:02:11 -0700

    Complete video 260: Promisifying the Geolocation API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,58 +7,54 @@ const countriesContainer = document.querySelector('.countries');
 // https://restcountries.com/v2/
 
 ///////////////////////////////////////////////////////////
-// VIDEO259 - Building a Simple Promise
+// VIDEO260 - Promisifying the Geolocation API
 
-// Promises accept 1 argument called the 'executor function'
-// The executor function will run immediately when the Promise is called
-// The executor function accepts 2 arguments: a resolve & reject function (named whatever)
-// The executor function contains the asynch behavior that we want to handle in the promise
-// The executor function should eventually produce a result value for the promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Waiting for draw...');
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    // Two callback functions, a resolve and reject
+    // Resolve returns the position from getCurrentPosition()
+    // Reject returns any errors
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
 
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      // resolve() sets the result of the promise to fulfilled (aka resolved)
-      // Whatever we pass into the resolve() function will become the result that will be used
-      // in the .then() handler of the promise
-      resolve('You Win 🦑');
-    } else {
-      // reject sets the result of the promise to rejected
-      reject(new Error('You LOSE 😟'));
-    }
-  }, 2000);
-});
-
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
-
-// Promisifying
-// Act of putting a promise inside of a callback function
-// Promisifying setTimeout callback function
-// Makes a function work like the fetch() function, by returning a promise
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
+    // Same as above
+    // The behavior is the same because the return value of a resolved getCurrentPosition() call is a position, and a rejected call returns an error
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
-};
+}
 
-const waitAF = function (seconds) {
-  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-};
-
-wait(3).then(res => console.log('Wait!'));
-waitAF(5).then(res => console.log('Wait AF!'));
-
-wait(7)
-  .then(() => {
-    console.log('I waited for 7 seconds');
-    return wait(1);
-  })
-  .then(() => console.log('I waited for 1 second'));
-
-// Immediately Resolving or Rejecting a Promise()
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+function whereAmI() {
+  getPosition()
+    .then(position => {
+      const { latitude: lat, longitude: lng } = position.coords;
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(response => {
+      if (response.status === 403)
+        throw new Error('Too many requests. Please wait');
+      if (!response.ok)
+        throw new Error(`Something went wrong: (${response.status})`);
+      return response.json();
+    })
+    .then(data => {
+      if (data.error?.code === '018') throw new Error('Coordinates not valid');
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Something went wrong: (${response.status})`);
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => {
+      console.error(`Error: ${err.message} 🎁`);
+      renderError(`Error: ${err.message} 🎁`);
+      renderContainer();
+    })
+    .finally(renderContainer());
+}
 
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
@@ -116,6 +112,4 @@ function renderError(msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
 }
 
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
+btn.addEventListener('click', whereAmI);
