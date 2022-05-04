2c00bf83689d0087021f996b8a71127efdddd851
FreezerFog <djwaccount+github@protonmail.com>
2022-05-04 13:13:38 -0700

    Complete 274: The Module Pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,33 +1,50 @@
 ///////////////////////////////////////////////////////////
-// VIDEO273: Top-level Await (ES2022)
+// VIDEO274: The Module Pattern
 
-// Top Level Await
-// Previously would have required an async function
-// Blocks execution of entire rest of module
-// Blocks execution of modules it exports to (until it finishes blocking code)
-// Use with caution!
+// The way to create & use modules before ES6
+// Useful to know because we will see this pattern in use
+// Primarily done using IIFE 'Iffy' functions
+// Works because of closures
+// Doesn't work well with multiple files due to needing to load HTML scripts in proper order
+// Bundling does not work with this approach
 
-// console.log('Start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// console.log('End fetching');
+const ShoppingCart = (function () {
+  const cart = [];
+  let shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
 
-async function getLastPost() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
-  console.log(data);
+  function addToCart(product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart. Shipping total ${shippingCost}`
+    );
+    shippingCost += 5;
+  }
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
-}
+  function orderStock(cart) {
+    cart.forEach(item => {
+      const { product, quantity } = item;
+      if (quantity === 1) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+      } else {
+        console.log(`${quantity} ${product}'s ordered from supplier`);
+      }
+    });
+  }
 
-// Consuming promise using top level await
-const lastPost = await getLastPost();
-console.log(lastPost);
+  return {
+    addToCart,
+    orderStock,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
 
-// Consuming promise using then
-// Not clean
-const lastPostThen = getLastPost();
-lastPostThen.then(last => console.log(last));
-
-import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('Apple', 4);
+ShoppingCart.addToCart('Pizza', 2);
+ShoppingCart.addToCart('Box', 1);
+console.log(ShoppingCart);
+console.log(ShoppingCart.shippingCost); // Return undefined, shippingCost was not returned
+ShoppingCart.orderStock(ShoppingCart.cart);
