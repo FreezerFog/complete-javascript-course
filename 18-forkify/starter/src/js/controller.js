1a3e4b2fa810b12550a806219c66b1571c1a24f1
FreezerFog <djwaccount+github@protonmail.com>
2022-05-06 15:04:50 -0700

    Complete video 288: Loading a Recipe from API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////////////////////////
+// VIDEO288: Loading a Recipe from API
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +14,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+async function showRecipe() {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcac4`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`(${res.status}) ${data.message}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(`${err}`);
+  }
+}
+
+showRecipe();
