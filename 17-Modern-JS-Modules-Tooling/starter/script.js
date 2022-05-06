cd7a7fecc80014246bbdd4c59cfbbef0849e9411
FreezerFog <djwaccount+github@protonmail.com>
2022-05-05 19:07:06 -0700

    Complete video 279: Configuring Babel & Polyfilling

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,9 +1,14 @@
 ///////////////////////////////////////////////////////////
-// VIDEO278: Bundling with Parcel and NPM Scripts
+// VIDEO279: Configuring Babel & Polyfilling
 
-// Run npm install in order to get the required node_modules
-// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-// Same as above, but Parcel will automatically find path for the import
+// Parcel uses Babel by default for code transpiling
+// Babel is used to transpile ES6 code into ES5 & older compatible code
+// For polyfilling new ES6 features (i.e not just syntax changes) to work with ES5 & older JS
+import 'core-js/stable';
+// For polyfilling async functions
+import 'regenerator-runtime/runtime';
+
+//
 import cloneDeep from 'lodash-es';
 import add, { cart } from './shoppingCart.js';
 
